a0e0bd593f1899581c4716d8e8b00aeeb06ee989
Ewusie-Isaac <owusu.lds@gmail.com>
2023-07-04 03:33:54 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Pizza from "./pizza";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <h1>Hello Pizza World</h1>
+      <Pizza />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pizza.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="Pizza Focaccia" />
+    </div>
+  );
+}
